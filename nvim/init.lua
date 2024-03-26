b0bd15cae30f955675261a31856e6df503d08e15
Sahil Yeole <sahilyeole93@gmail.com>
2024-03-26 14:24:10 +0530

    add barbecue and flutter tools

--- nvim/init.lua
+++ nvim/init.lua
@@ -66,7 +66,15 @@ require('lazy').setup {
   { 'numToStr/Comment.nvim', opts = {} },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false, keywords = {
+      TODO = { color = 'info' },
+      NOTE = { color = 'hint' },
+    } },
+  },
 
   {
     'wakatime/vim-wakatime',
@@ -74,6 +82,66 @@ require('lazy').setup {
     setup = function()
       vim.cmd [[packadd wakatime/vim-wakatime]]
     end,
+  },
+
+  { 'github/copilot.vim' },
+
+  {
+    'utilyre/barbecue.nvim',
+    name = 'barbecue',
+    version = '*',
+    dependencies = {
+      'SmiteshP/nvim-navic',
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {},
+  },
+  { 'mfussenegger/nvim-dap' },
+  {
+    'akinsho/flutter-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'stevearc/dressing.nvim' },
+    config = function()
+      require('flutter-tools').setup {
+        -- (uncomment below line for windows only)
+        -- flutter_path = "home/flutter/bin/flutter.bat",
+
+        debugger = {
+          -- make these two params true to enable debug mode
+          enabled = false,
+          run_via_dap = false,
+          register_configurations = function(_)
+            require('dap').adapters.dart = {
+              type = 'executable',
+              command = vim.fn.stdpath 'data' .. '/mason/bin/dart-debug-adapter',
+              args = { 'flutter' },
+            }
+
+            require('dap').configurations.dart = {
+              {
+                type = 'dart',
+                request = 'launch',
+                name = 'Launch flutter',
+                dartSdkPath = 'home/flutter/bin/cache/dart-sdk/',
+                flutterSdkPath = 'home/flutter',
+                program = '${workspaceFolder}/lib/main.dart',
+                cwd = '${workspaceFolder}',
+              },
+            }
+            -- uncomment below line if you've launch.json file already in your vscode setup
+            -- require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        dev_log = {
+          -- toggle it when you run without DAP
+          enabled = false,
+          open_cmd = 'tabedit',
+        },
+      }
+    end,
+  },
+  -- for dart syntax hightling
+  {
+    'dart-lang/dart-vim-plugin',
   },
 
   { import = 'plugins' },
