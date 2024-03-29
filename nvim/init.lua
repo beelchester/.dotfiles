5317c796539a5f99ae5ebeb5ada7ae2fa67c79a3
Sahil Yeole <sahilyeole93@gmail.com>
2024-03-29 15:44:31 +0530

    git update and dvorak

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 -- vim.cmd 'autocmd VimEnter * :Neotree toggle'
 vim.cmd 'autocmd VimEnter * :Alpha'
-vim.keymap.set({ 'n', 'v', 'i', 'x' }, '<M-w>', '<C-w>')
+vim.keymap.set({ 'n', 'v', 'i', 'x' }, '<M-w>', '<C-,>')
 
 vim.opt.conceallevel = 1
 vim.opt.number = true
--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,5 +3,6 @@ return {
   opts = {},
   config = function()
     vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'fugitive' })
+    vim.keymap.set('n', '<leader>cc', ':Git commit --signoff<CR>', { desc = 'commit with signoff' })
   end,
 }
